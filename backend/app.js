6e29d1db26c399e9497b8b8e1170bace32280d31
AudreyErza <audrey.revenault@gmail.com>
2024-02-22 14:40:14 +0100

    session

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const userRoutes = require('./routes/user');
-// const sauceRoutes = require('./routes/sauce');
+const sauceRoutes = require('./routes/sauce');
 
 mongoose.connect('mongodb+srv://byaudreyyy:ludo@cluster0.ph2qypv.mongodb.net/piquante?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -11,7 +11,6 @@ mongoose.connect('mongodb+srv://byaudreyyy:ludo@cluster0.ph2qypv.mongodb.net/piq
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -21,6 +20,8 @@ app.use((req, res, next) => {
   });
 
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
-// app.use('/api/sauces', sauceRoutes);
+
 module.exports = app;
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,17 +1,17 @@
-const sauce = require('../models/sauce');
+const Sauce = require('../models/sauce');
 
-exports.getAllsauces = (req, res,) => {
-    sauce.find()
+exports.getAllSauces = (req, res,next) => {
+    Sauce.find()
     .then((sauce)=>res.status(200).json({sauce}))
     .catch(error => res.status(400).json({error}))
 };
-exports.createSauce = (req, res,) => {
+exports.createSauce = (req, res,next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
-    delete sauceObject._userId;
+    // delete sauceObject._userId;
     const sauce = new Sauce({
         ...sauceObject,
-        userId: req.auth.userId,
+        // userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
@@ -21,8 +21,12 @@ console.log = "Objet créé"
 };
 
 
-// exports.getOneSauce = (req, res,) => {};
-exports.modifySauce = (req, res,) => {
+exports.getOneSauce = (req, res,next) => {
+    Sauce.findOne({_id: req.params.id})
+    .then((sauce) => res.status(200).json(sauce))
+    .catch(error => { res.status(400).json( { error })})
+};
+exports.modifySauce = (req, res,next) => {
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.Sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -34,7 +38,7 @@ exports.modifySauce = (req, res,) => {
             if (sauce.userId != req.auth.userId) {
                 res.status(401).json({ message : 'Not authorized'});
             } else {
-                sauce.updateOne({ _id: req.params.id}, { ...SauceObject, _id: req.params.id})
+                sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
                 .then(() => res.status(200).json({message : 'Objet modifié!'}))
                 .catch(error => res.status(401).json({ error }));
             }
@@ -42,20 +46,15 @@ exports.modifySauce = (req, res,) => {
         .catch((error) => {
             res.status(400).json({ error });
         });
- };
-exports.modifySauce = (req, res,) => {
-    const sauceObject = req.file ? {
-        ...JSON.parse(req.body.Sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-} 
-exports.deleteSauce = (req, res,) => {
-    sauce.findOne({_id: req.params.id})
+};
+
+exports.deleteSauce = (req, res,next) => {
+    Sauce.findOne({_id: req.params.id})
         .then((sauce) => {
             if (sauce.userId != req.auth.userId) {
                 res.status(401).json({ message : 'Not authorized'});
             } else {
-                sauce.updateOne({ _id: req.params.id}, { ...SauceObject, _id: req.params.id})
+                sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
                 .then(() => res.status(200).json({message : 'Objet modifié!'}))
                 .catch(error => res.status(401).json({ error }));
             }
