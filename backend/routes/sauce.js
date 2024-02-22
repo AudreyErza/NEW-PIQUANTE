5588879abe8f13dd38f51fe33a22acd8dc9408e0
AudreyErza <audrey.revenault@gmail.com>
2024-02-22 12:02:19 +0100

    avant soutenance du 220224
    Ajout de sauce = l'image s'ajoute et le dossier s'est créé dans Mongo mais pas la sauce.

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,7 +8,7 @@ const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', sauceCtrl.getAllsauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+// router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
