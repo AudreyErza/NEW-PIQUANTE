352dd3e16b1497aca9944a23a66d44e05ec7a6e1
AudreyErza <audrey.revenault@gmail.com>
2024-03-14 10:21:58 +0100

    14/03 matin
    Like et dislike OK

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -20,10 +20,8 @@ exports.getOneSauce = (req, res, next) => {
 exports.createSauce = (req, res,next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
-    // delete sauceObject._userId;
     const sauce = new Sauce({
         ...sauceObject,
-        // userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
@@ -32,7 +30,7 @@ exports.createSauce = (req, res,next) => {
 console.log = "Objet créé"
 };
 
-//Afficher l'ensemble des sauces sur la page d'accueil- KO //
+//Modifier une sauce//
 exports.modifySauce = (req, res,next) => {
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
@@ -77,3 +75,68 @@ exports.deleteSauce = (req, res, next) => {
             res.status(500).json({ error });
         });
 };
+
+// LIKE OR DISLIKE Sauce
+exports.likeSauce = (req, res, next) => {
+    // Lorsque l'utilisateur clique sur le like//
+    // Ajout d'un like + ajout de l'identifiant utilisateur dans le tableau des usersLiked//
+    if (req.body.like === 1) {
+    Sauce.updateOne(
+        { _id: req.params.id },
+        {
+        $inc: { likes: req.body.like++ },
+        $push: { usersLiked: req.body.userId },
+        }
+      )
+        .then((sauce) => res.status(200).json({ message: "Un like de plus !" }))
+        .catch((error) => res.status(400).json({ error }));
+    }
+  
+      // Lorsque l'utilisateur clique sur le unlike//
+    //suppression d'un like + ajout de l'identifiant utilisateur dans le tableau des usersDisliked//
+    else if (req.body.like === -1) {
+      Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { dislikes: req.body.like++ * -1 },
+          $push: { usersDisliked: req.body.userId },
+        }
+      )
+        .then((sauce) =>
+          res.status(200).json({ message: "Un dislike de plus !" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    }
+  
+    // Si l'utilisateur reclique sur like ou dislike //
+    // Retire le like ou le dislike et l'identifiant de l'utilisateur dans le tableau //
+    else {
+      Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauce.updateOne(
+              { _id: req.params.id },
+              { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: "Un like de moins !" });
+              })
+              .catch((error) => res.status(400).json({ error }));
+          } else if (sauce.usersDisliked.includes(req.body.userId)) {
+            Sauce.updateOne(
+              { _id: req.params.id },
+              {
+                $pull: { usersDisliked: req.body.userId },
+                $inc: { dislikes: -1 },
+              }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: "Un dislike de moins !" });
+              })
+              .catch((error) => res.status(400).json({ error }));
+          }
+        })
+        .catch((error) => res.status(400).json({ error }));
+    }
+  };
+  
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -7,9 +7,11 @@ const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
 
 module.exports = router;
